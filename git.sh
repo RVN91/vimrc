f9b10dc0a511377e69113798c18e5ea3d6578c4e
Rasmus Vest Nielsen <rn@civil.aau.dk>
2021-01-03 18:58:31 +0100

    fixin nerdtree

--- git.sh
+++ git.sh
@@ -4,10 +4,7 @@ timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
 }
 git commit -am "Regular auto-commit $(timestamp)"
-git push --all
-
-Enter username for https://github.com: rvn91
-Enter password for https://github.com: Rhenium186
+git push origin master
 
 rm -R ~/.vim_runtime
 git clone --depth=1 https://github.com/rvn91/vimrc.git ~/.vim_runtime
