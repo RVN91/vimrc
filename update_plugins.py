d065717d5b73aa119f82c75e5b64dab5ed9cea1c
Rasmus Vest Nielsen <rn@civil.aau.dk>
2021-01-03 19:47:54 +0100

    Regular auto-commit at 19:47:54 on 03/01/2021

--- update_plugins.py
+++ update_plugins.py
@@ -24,7 +24,6 @@ GITHUB_ZIP = "%s/archive/master.zip"
 
 SOURCE_DIR = path.join(path.dirname(__file__), "plugins")
 
-
 def download_extract_replace(plugin_name, folder_name, zip_path, temp_dir, source_dir):
     temp_zip_path = path.join(temp_dir, plugin_name)
 
@@ -50,7 +49,6 @@ def download_extract_replace(plugin_name, folder_name, zip_path, temp_dir, sourc
     shutil.move(plugin_temp_path, plugin_dest_path)
     print("Updated {0}".format(plugin_name))
 
-
 def update(plugin):
     folder, github_url = plugin.split(" ")
     name = github_url.split("/")[-1]
@@ -58,7 +56,6 @@ def update(plugin):
     zip_path = GITHUB_ZIP % github_url
     download_extract_replace(name, folder, zip_path, temp_directory, SOURCE_DIR)
     
-
 if __name__ == "__main__":
     temp_directory = tempfile.mkdtemp()
 
