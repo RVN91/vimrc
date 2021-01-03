a0aa84c6c3f6c69ef7ac2387739d372732e86a22
Rasmus Vest Nielsen <rn@civil.aau.dk>
2021-01-03 19:00:09 +0100

    Regular auto-commit at 19:00:09 on 03/01/2021

--- git.sh
+++ git.sh
@@ -6,5 +6,5 @@ timestamp() {
 git commit -am "Regular auto-commit $(timestamp)"
 git push origin master
 
-rm -R ~/.vim_runtime
+sudo rm -R ~/.vim_runtime
 git clone --depth=1 https://github.com/rvn91/vimrc.git ~/.vim_runtime
