36f1bfc90e0c6f2705cb6697fd93f29ccc2944f2
Rasmus Vest Nielsen <rn@civil.aau.dk>
2021-01-03 18:52:23 +0100

    Regular auto-commit at 18:52:23 on 03/01/2021

--- git.sh
+++ git.sh
@@ -6,4 +6,8 @@ timestamp() {
 git commit -am "Regular auto-commit $(timestamp)"
 git push origin master
 
+Enter username for https://github.com: rvn91
+Enter password for https://github.com: Rhenium186
+
+rm -R ~/.vim_runtime
 git clone --depth=1 https://github.com/rvn91/vimrc.git ~/.vim_runtime
